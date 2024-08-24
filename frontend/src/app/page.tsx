ea296344f6c23d82144b9376c6f5825dc1099f48
Aira Jena <airajena.hack@gmail.com>
2024-08-24 22:30:27 +0530

    feat: Add hero section to the app

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/base/HeroSection";
 import Navbar from "@/components/base/Navbar";
 
 export default async function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col ">
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
