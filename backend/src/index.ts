0be81e2bc45d385796a0decd3906e10827617c50
Aira Jena <airajena.hack@gmail.com>
2024-08-25 00:39:50 +0530

    feat: Set up prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 const app: Application = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
