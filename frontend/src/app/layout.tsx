179ce99ea7c1555e8174649d9ad7208ca5d87a6e
Aira Jena <airajena.hack@gmail.com>
2024-08-27 16:11:48 +0530

    feat: Add sonner toast

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import SessionProvider from "@/providers/SessionProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           )}
         >
           {children}
+          <Toaster richColors duration={5000} />
         </body>
       </SessionProvider>
     </html>
