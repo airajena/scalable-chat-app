bc72e8b4d6959d1c4ce636c447a76eda84d98f67
Aira Jena <airajena.hack@gmail.com>
2024-08-30 23:46:20 +0530

    feat: Enhance socket connection handling with room-based message broadcasting

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,9 +1,24 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
+interface CustomSocket extends Socket {
+  room?: string;
+}
 export function setupSocket(io: Server) {
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room || socket.handshake.headers.room;
+    if (!room) {
+      return next(new Error("Invalid room"));
+    }
+    socket.room = room;
+    next();
+  });
 
-  io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+  io.on("connection", (socket: CustomSocket) => {
+    socket.join(socket.room);
+
+    socket.on("message", async (data) => {
+      socket.to(socket.room).emit("message", data);
+    });
 
     socket.on("disconnect", () => {
       console.log("A user disconnected:", socket.id);
