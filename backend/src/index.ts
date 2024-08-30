db276f6a31426ef3d9ef53a0d653f52b76ff7ede
Aira Jena <airajena.hack@gmail.com>
2024-08-30 23:43:53 +0530

    feat: Add Socket.IO Admin UI instrumentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,14 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 const app: Application = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7000;
 import Routes from "./routes/index.js";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import { setupSocket } from "./socket.js";
 import { createAdapter } from "@socket.io/redis-streams-adapter";
 import redis from "./config/redis.js";
-
+import { instrument } from "@socket.io/admin-ui";
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -17,6 +17,11 @@ const io = new Server(server, {
     origin: [process.env.CLIENT_APP_URL, "https://admin.socket.io"],
   },
   adapter: createAdapter(redis),
+});
+
+instrument(io, {
+  auth: false,
+  mode: "development",
 });
 
 export { io };
@@ -27,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("It's working Guys 🙌");
 });
 
 app.use("/api", Routes);
